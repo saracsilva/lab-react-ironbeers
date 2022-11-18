c54d02e96a49da0c7a4cf975bbafdd6d974dade7
saracsilva <saracarolinasilva@gmail.com>
2022-11-18 16:06:14 +0100

    iteration_5 done

--- src/components/RandomBeer.jsx
+++ src/components/RandomBeer.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
+import axios from "axios";
 
 function RandomBeer() {
+  const [randomBeer, setRandomBeer] = useState([]);
+
+  const fetchBeer = async (setter) => {
+    const response = await axios.get(
+      `https://ih-beers-api2.herokuapp.com/beers/random`
+    );
+    setter(response.data);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchBeer(setRandomBeer);
+  }, []);
   return (
     <div>
       <Header />
-      RandomBeer
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={randomBeer.image_url} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h1 className="card-title">{randomBeer.name}</h1>
+          <h4 className="card-text">{randomBeer.tagline}</h4>
+          <h4 className="card-text">{randomBeer.first_brewed}</h4>
+          <h4 className="card-text">{randomBeer.attenuation_level}</h4>
+          <p className="card-text">{randomBeer.description}</p>
+          <h6 className="card-text">
+            <b>Created By:</b>
+            {randomBeer.contributed_by}
+          </h6>
+        </div>
+      </div>
     </div>
   );
 }
